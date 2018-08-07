1cabe57938d3b50d5d2da23fbb0e6aef15c1a958
bluestar92011 <bluestar92011@gmail.com>
2018-08-07 20:49:44 +0000

    automatically backed up by learn

---
+++ loop.rb
@@ -0,0 +1,5 @@
+def alarm_clock_loreen
+  time = 6
+  until time += 8
+  puts "Wake up"
+  time += 7
---
+++ loops.rb
@@ -0,0 +1,20 @@
+
+  loop do
+    puts "Now watch me whip"
+    puts "whatch me nae nae"
+    
+  end
+ 
+ 
+ def alarm_clock_noor
+   time = 30
+   while time <= 60
+   puts "Wake Up!"
+   time += 9
+   
+   
+ d  
+ end 
+ 
+ 
+ alarm_clock_noor
